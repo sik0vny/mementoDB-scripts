140eca599bd604fadf57655373375a5a016b10c4
sik0vny <40767185+sik0vny@users.noreply.github.com>
2020-10-21 22:46:55 +0200

    Update kml.js

--- android/kml.js
+++ android/kml.js
@@ -4,8 +4,61 @@ var Kml = {
     @param {string} latlon
     @param {string} altitude
 
-    */
-    generate: function(latlon, altitude) {
-        message("test");
+var entries = selectedEntries();
+
+    var c = '<?xml version="1.0" encoding="UTF-8"?>\n';
+    c += '<kml xmlns="http://www.opengis.net/kml/2.2">\n';
+
+    for (i = 0; i < entries.length; i++) {
+      var e = entries[i];
+
+      let strain = e.field("Strain");
+
+      let acc = e.field("Accuracy");
+
+      let alt = e.field("Altitude");
+      if (alt == null) { alt = "0"; }
+
+      var coords = e.field("location") + "";
+      coords = coords.split(",");
+      coords = coords[1] + "," + coords[0] + "," + alt;
+
+      let datetime = moment(e.field("datetime")).format("HH:mm YY.MM.DD");
+
+      let desc = "Time: " + datetime + "\n";
+      desc += "GPS Accuracy: " + acc + "m";
+
+      c += "  <Placemark>\n";
+      c += "    <name>" + strain + "</name>\n";
+      c += "     <description>" + desc + "</description>\n";
+      c += "     <Point>\n";
+      c += "       <coordinates>" + coords + "</coordinates>\n";
+      c += "     </Point>\n";
+      c += "  </Placemark>\n";
     }
+
+    c += "</kml>";
+
+    f = file(path);
+    f.write(c);
+    f.close();
+
+    message(c);
+    message("Exported " + entries.length + " houby");
+
+
+
+    */
+  placemark: function(name, desc, latlon, altitude) {
+    var coords = latlon.split(",");
+    coords = coords[1] + "," + coords[0] + "," + altitude;
+    
+    c =  "  <Placemark>\n";
+    c += "    <name>" + name + "</name>\n";
+    c += "     <description>" + desc + "</description>\n";
+    c += "     <Point>\n";
+    c += "       <coordinates>" + coords + "</coordinates>\n";
+    c += "     </Point>\n";
+    c += "  </Placemark>\n";
+  }
 }
