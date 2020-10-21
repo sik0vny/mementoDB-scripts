4c762135fe0f307398f0bcaf99605fd738a32ca0
sik0vny <40767185+sik0vny@users.noreply.github.com>
2020-10-21 23:22:11 +0200

    Update kml.js

--- android/kml.js
+++ android/kml.js
@@ -4,29 +4,7 @@ var Kml = {
   
   
   
-  placemark: function(name, desc, latlon, altitude) {
-    var coords = latlon.split(",");
-    coords = coords[1] + "," + coords[0] + "," + altitude;
-    
-    result =  "  <Placemark>\n";
-    result += "    <name>" + name + "</name>\n";
-    result += "     <description>" + desc + "</description>\n";
-    result += "     <Point>\n";
-    result += "       <coordinates>" + coords + "</coordinates>\n";
-    result += "     </Point>\n";
-    result += "  </Placemark>\n";
-    return result; 
-  }
-  
-  kml: function(name, contents) {
-    //result  = '<?xml version="1.0" encoding="UTF-8"?>';
-    //result += '<kml xmlns="http://www.opengis.net/kml/2.2">';
-    result += "<Document>"
-    result += contents;
-    result += "</Document>";
-    result += "</kml>";
-    return result;
-  }
+
   
   /**  
   Generate a kml file
